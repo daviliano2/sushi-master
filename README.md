7eda7fae1d76586019a49f8785a99c7ba1ef467b
david_gh <david.granados.hernandez@perseus.de>
2021-05-08 20:32:53 +0200

    Add all logic related to orders and payment calculations.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# sushi-master
+# Sushi Master Test
 
-1. npm install
-2. npm run start
+## How to run the application
+1. Clone the repo and get into the folder.
+2. Run `npm install`
+3. Then `npm run start`
+
+## Tools used
+- Webpack and webpack-dev-server for building and runing locally the application
+- Bootstrap for the styling
+
+## Thought process
+The restriction of not using a framework like React made me think that maybe most of the HTML had to be written down manually and then modify the content with JS but since we are using data from and external source, the content could change, for example with more items in the menu.
+
+This is why I decided to create a basic HTML skeleton and populate it dynamically using the data in the JSON and javacript.
+
+I tried to keep the domains separated in a logical way, functions that create the layout are all in the `layoutHelpers` file while most of the business logic is located in `formHelpers`.
+
+I didn't really make use of form methods or even submitted the data since it was not really especified in the instructions, that part was a bit ambiguous.
+
+Things I would have done differently in other situations.
+- If frameworks were allowed:
+  - I would have worked on a better UI with `Material-UI`
+  - I would have use `Typescript` and a form library like `React Hook Form`
+  - I would have written tests most probably with `Testing Library`
--- src/layoutHelpers.js
+++ src/layoutHelpers.js
@@ -1,9 +1,17 @@
+import { roundDecimals } from './formHelpers'
 import data from './data.json'
 
-export function createCustomElement({ htmlTag = 'div', className = null } = {}) {
+export function createCustomElement({
+  htmlTag = 'div',
+  className = null,
+  text = null,
+  type = null,
+} = {}) {
   const element = document.createElement(htmlTag)
 
   if (className) element.className = className
+  if (text) element.innerText = text
+  if (type) element.type = type
 
   return element
 }
@@ -12,15 +20,16 @@ export function createProductCard(productName, productDescription) {
   const card = createCustomElement({ className: 'card' })
   const cardBody = createCustomElement({ className: 'card-body' })
   const textWrapper = createCustomElement()
-  const divProductName = createCustomElement()
-  const divProductDescription = createCustomElement()
-  const addButton = createCustomElement({ htmlTag: 'button', className: 'btn btn-primary' })
+  const divProductName = createCustomElement({ text: productName })
+  const divProductDescription = createCustomElement({ text: productDescription })
+  const addButton = createCustomElement({
+    htmlTag: 'button',
+    className: 'btn btn-primary',
+    text: '+',
+    type: 'button',
+  })
 
-  divProductName.innerText = productName
-  divProductDescription.innerText = productDescription
   addButton.id = `button-${productName}`
-  addButton.type = 'button'
-  addButton.innerText = '+'
   addButton.value = productName
 
   textWrapper.appendChild(divProductName)
@@ -32,33 +41,39 @@ export function createProductCard(productName, productDescription) {
   return card
 }
 
-export function createOrderCard({ productName, price }) {
+export function createOrderRow({ productName, price }) {
+  const sizes = data.size_multipliers
   const cardBody = createCustomElement({ className: 'card-body' })
-  const divProductName = createCustomElement({ className: 'col' })
-  const divProductSize = createCustomElement({ htmlTag: 'select', className: 'form-select col' })
+  const divProductName = createCustomElement({ className: 'col', text: productName })
+  const selectProductSize = createCustomElement({ htmlTag: 'select', className: 'form-select col' })
   const divProductPrice = createCustomElement({ className: 'col align-right' })
+  const spanPrice = createCustomElement({ htmlTag: 'span', text: price })
+  spanPrice.setAttribute('data-size', 'Small')
+  const spanCurrency = createCustomElement({ htmlTag: 'span', text: ' €' })
+  const inputProductPriceValue = createCustomElement({
+    htmlTag: 'input',
+    type: 'hidden',
+  })
   const removeButton = createCustomElement({
     htmlTag: 'button',
     className: 'btn btn-outline-danger btn-sm',
+    text: 'x',
+    type: 'button',
   })
 
-  const sizes = data.size_multipliers
-
-  Object.keys(sizes).forEach(size => {
-    const selectOption = createCustomElement({htmlTag: 'option'})
+  Object.keys(sizes).forEach((size) => {
+    const selectOption = createCustomElement({ htmlTag: 'option', text: size })
     selectOption.value = sizes[size]
-    selectOption.innerText = size
-    divProductSize.appendChild(selectOption)
+    inputProductPriceValue.value = price
+    selectProductSize.appendChild(selectOption)
   })
 
-  divProductName.innerText = productName
-  divProductPrice.innerText = price
-  removeButton.type = 'button'
-  removeButton.innerText = 'x'
-
+  divProductPrice.appendChild(spanPrice)
+  divProductPrice.appendChild(spanCurrency)
   divProductPrice.appendChild(removeButton)
+  divProductPrice.appendChild(inputProductPriceValue)
   cardBody.appendChild(divProductName)
-  cardBody.appendChild(divProductSize)
+  cardBody.appendChild(selectProductSize)
   cardBody.appendChild(divProductPrice)
 
   return cardBody
